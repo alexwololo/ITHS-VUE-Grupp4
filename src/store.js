637e9a4a313294b24bec0c74e47f4ba256bb66a1
Ludwig Nojd <ludwig.nojd@hotmail.se>
2022-02-09 09:18:50 +0100

    VueX update

--- src/store.js
+++ src/store.js
@@ -1,8 +1,33 @@
+// import { createStore } from 'vuex'
+// import VuexPersistence from 'vuex-persist'
+// const vuexLocal = new VuexPersistence({
+//   storage: window.localStorage,
+
+//   mutations: {
+//     setUser(state, user) {
+//       state.user = user
+//     },
+//     setPassword(state, password) {
+//       state.password = password
+//     },
+//     setEmail(state, email) {
+//       state.email = email
+//     }
+//   },
+//   state: {
+//     user: {},
+//     password: {},
+//     email: {}
+//   },
+//   plugins: [vuexLocal.plugin]
+// })
+// export default createStore({ vuexLocal, strict: true })
+
 import { createStore } from 'vuex'
 import VuexPersistence from 'vuex-persist'
-const vuexLocal = new VuexPersistence({
-  storage: window.localStorage,
-
+console.log(new VuexPersistence({ storage: localStorage }))
+export default
+createStore({
   mutations: {
     setUser(state, user) {
       state.user = user
@@ -14,11 +39,7 @@ const vuexLocal = new VuexPersistence({
       state.email = email
     }
   },
-  state: {
-    user: {},
-    password: {},
-    email: {}
-  },
-  plugins: [vuexLocal.plugin]
+  plugins: [new VuexPersistence({ storage: localStorage }).plugin],
+  state: { user: {}, password: {}, email: {} },
+  strict: true
 })
-export default createStore({ vuexLocal, strict: true })
