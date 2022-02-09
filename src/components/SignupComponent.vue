d5ff4164474ae7e788caa671c8f72331e163db44
Ludwig Nojd <ludwig.nojd@hotmail.se>
2022-02-09 10:27:14 +0100

    New Component

---
+++ src/components/SignupComponent.vue
@@ -0,0 +1,48 @@
+<script>
+  export default {
+    data() {
+      return { s: '', t: '', u: '' }
+    },
+    methods: {
+      regUser() {
+        this.$store.commit('setUser', this.s)
+        this.$store.commit('setEmail', this.t)
+        this.$store.commit('setPassword', this.u)
+        console.log(this.s)
+      }
+    }
+  }
+</script>
+
+<template>
+  <RouterView />
+  <h1>Registrering</h1>
+  <div class="wrapper">
+    <form>
+      <label for="username">Användarnamn:</label>
+      <input
+        v-model="s"
+        id="username"
+        type="text"
+        placeholder="Användarnamn"
+      />
+      <br />
+      <label for="email">Email:</label
+      ><input
+        v-model="t"
+        id="email"
+        type="text"
+        placeholder="Mail"
+      />
+      <br />
+      <label for="password">Lösen:</label
+      ><input
+        v-model="u"
+        id="password"
+        type="password"
+        placeholder="Lösen"
+      />
+      <input @click="regUser" type="button" value="Registrera" />
+    </form>
+  </div>
+</template>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,52 +1,12 @@
 <script>
-  export default {
-    data() {
-      return { userId: this.$store.state.user, s: '', t: '', u: '' }
-    },
-    methods: {
-      blabla() {
-        this.$store.commit('setUser', this.s)
-        this.$store.commit('setEmail', this.t)
-        this.$store.commit('setPassword', this.u)
-        console.log(this.s)
-        console.log(this.userId)
-      }
-    }
+import SignupComponent from "../components/SignupComponent.vue";
+export default {
+  components: {
+    SignupComponent
   }
+}
 </script>
 
 <template>
-  <RouterView />
-  <h1>Registrering</h1>
-  <div class="wrapper">
-    <form>
-      <label for="username">Användarnamn:</label>
-      <input
-        @input="blabla"
-        v-model="s"
-        id="username"
-        type="text"
-        placeholder="Användarnamn"
-      />
-      <br />
-      <label for="email">Email:</label
-      ><input
-        @input="blabla"
-        v-model="t"
-        id="email"
-        type="text"
-        placeholder="Mail"
-      />
-      <br />
-      <label for="password">Lösen:</label
-      ><input
-        @input="blabla"
-        v-model="u"
-        id="password"
-        type="password"
-        placeholder="Lösen"
-      />
-    </form>
-    <button class="button">Registrera</button>
-  </div>
+<SignupComponent />
 </template>
