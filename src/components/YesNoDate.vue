c3248b7eeabc11f080265358ba0a7381b19006e4
Caroline <caroline.hajosi@iths.se>
2022-02-09 08:29:08 +0100

    New yes/no-component

---
+++ src/components/YesNoDate.vue
@@ -0,0 +1,32 @@
+<script>
+  export default {
+    methods: {
+      onYes() {
+        alert('Yes')
+      },
+      onNo() {
+        alert('No')
+      }
+    }
+  }
+</script>
+
+<template>
+  <div @click="onYes" type="button" value="Yes?">
+    <img alt="" src="/assets/thumb-up.svg" />
+  </div>
+  <div @click="onNo" type="button" value="No?">
+    <img alt="" src="/assets/thumb-down.svg" />
+  </div>
+</template>
+
+<style scoped>
+  img {
+    width: 10%;
+
+    background-color: aliceblue;
+    border-radius: 60px;
+    margin: 50px;
+    border: solid aliceblue 20px;
+  }
+</style>
--- src/views/DateView.vue
+++ src/views/DateView.vue
@@ -1,13 +1,16 @@
 <template>
   <CountDown />
+  <YesNoDate />
 </template>
 
 <script>
   import CountDown from '../components/CountDown.vue'
+  import YesNoDate from '../components/YesNoDate.vue'
 
   export default {
     components: {
-      CountDown
+      CountDown,
+      YesNoDate
     }
   }
 </script>
