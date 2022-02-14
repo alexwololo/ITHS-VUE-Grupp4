5c0c652d5661c3a9ea71e845aad125c5c25a055e
AMD-93 <amoyad93@gmail.com>
2022-02-14 15:33:12 +0100

    minor changes

--- src/components/ResponsiveNavigation.vue
+++ src/components/ResponsiveNavigation.vue
@@ -78,7 +78,7 @@
       margin-block-start: 0;
       margin-block-end: 0;
       padding-inline-start: 0;
-      padding-right: 30px;
+      padding-right: 0px;
 
       figure {
         cursor: pointer;
@@ -93,7 +93,7 @@
       }
 
       i {
-        margin-right: 10px;
+        margin-right: 40px;
         font-size: 22px;
       }
 
