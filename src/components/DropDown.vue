8212cf8cc01e54ea6c4462f477264824c8683e1f
Anton Walther <anton.walther@live.com>
2022-02-16 14:50:13 +0100

    Merge branch 'main' of https://github.com/alexwololo/ITHS-VUE-Grupp4

---
+++ src/components/DropDown.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="menu-item" @click="isOpen = !isOpen">
+    <a href="#">{{ title }}</a>
+    <transition name="fade" appear>
+      <div class="sub-menu" v-if="isOpen">
+        <div v-for="(item, i) in items" :key="i" class="menu-item">
+          <a :href="item.link"> {{ item.title }} </a>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DropDown',
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      items: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        isOpen: false
+      }
+    }
+  }
+</script>
+
+<style>
+  nav .menu-item .sub-menu {
+    position: absolute;
+    background-color: #222;
+    top: calc(100% + 18px);
+    left: 50%;
+    transform: translateX(-50%);
+    width: max-content;
+    border-radius: 0px 0px 16px 16px;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all 0.5s ease-out;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav>
+    <div class="menu-item"><a href="#">Home</a></div>
+    <div class="menu-item"><a href="#">About</a></div>
+    <DropDown title="Services" :items="services" />
+    <div class="menu-item"><a href="#">Contact</a></div>
+  </nav>
+</template>
+
+<script>
+  import DropDown from './DropDown.vue'
+
+  export default {
+    name: 'NavBar',
+    components: {
+      DropDown
+    },
+    data() {
+      return {
+        services: [
+          {
+            title: 'Web',
+            link: '#'
+          },
+          {
+            title: 'Design',
+            link: '#'
+          },
+          {
+            title: 'Videos',
+            link: '#'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  nav .menu-item {
+    color: #fff;
+    padding: 10px 20px;
+    position: relative;
+    text-align: center;
+    border-bottom: 3px solid transparent;
+    display: flex;
+    transition: 0.4s;
+  }
+
+  nav .menu-item.active,
+  nav .menu-item:hover {
+    background-color: #444;
+    border-bottom-color: #ff5858;
+  }
+  nav .menu-item a {
+    color: inherit;
+    text-decoration: none;
+  }
+</style>
