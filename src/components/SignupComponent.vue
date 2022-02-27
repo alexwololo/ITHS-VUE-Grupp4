1d7a347f2f1feafc5ffa260449239c0196481b15
alexwololo <alexander.caspersson@gmail.com>
2022-02-27 21:22:07 +0100

    accepted incoming change in dateview and removed testing text from signupcomponent

--- src/components/SignupComponent.vue
+++ src/components/SignupComponent.vue
@@ -2,36 +2,40 @@
   export default {
     emits: ['show-message'],
     data() {
-      return { message: false, s: '', t: '', u: '', msg: [],
-      usernameValid: false,
-      emailValid: false,
-      passwordValid: false,
-      isRegisterDisabled: true,}
+      return {
+        message: false,
+        s: '',
+        t: '',
+        u: '',
+        msg: [],
+        usernameValid: false,
+        emailValid: false,
+        passwordValid: false,
+        isRegisterDisabled: true
+      }
     },
     watch: {
-    t(value){
-      // binding this till data value i email input
-      this.email = value;
-      this.validateEmail(value);
-      this.formCheckValidation()
+      t(value) {
+        // binding this till data value i email input
+        this.email = value
+        this.validateEmail(value)
+        this.formCheckValidation()
       },
-    s(value){
-      this.username = value;
-      this.nameIsValid(value);
-      this.formCheckValidation()
+      s(value) {
+        this.username = value
+        this.nameIsValid(value)
+        this.formCheckValidation()
+      },
+      u(value) {
+        this.password = value
+        this.passwordIsValid(value)
+        this.formCheckValidation()
+      }
     },
-    u(value){
-      this.password = value;
-      this.passwordIsValid(value);
-      this.formCheckValidation()
-    }
-
-    },
-
 
     methods: {
       regUser() {
-        console.log("testing", 'register')
+        console.log('testing', 'register')
         this.$store.commit('setUser', this.s)
         this.$store.commit('setEmail', this.t)
         this.$store.commit('setPassword', this.u)
@@ -48,24 +52,24 @@
         this.$emit('show-message')
       },
 
-      validateEmail(value){
+      validateEmail(value) {
         // eslint-disable-next-line no-useless-escape
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)){
-          this.msg['email'] = '';
-          this.emailValid = true;
-        } else{
-          this.msg['email'] = 'Invalid Email Address';
-          this.emailValid = false;
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+          this.msg['email'] = ''
+          this.emailValid = true
+        } else {
+          this.msg['email'] = 'Invalid Email Address'
+          this.emailValid = false
         }
       },
       nameIsValid(value) {
         const username = value
-        if (username.length >= 5 && username.length <= 20){
-          this.msg['username'] = '';
-          this.usernameValid = true;
-        } else{
-          this.msg['username'] = 'Måste innehålla 5-20 tecken';
-          this.usernameValid = false;
+        if (username.length >= 5 && username.length <= 20) {
+          this.msg['username'] = ''
+          this.usernameValid = true
+        } else {
+          this.msg['username'] = 'Måste innehålla 5-20 tecken'
+          this.usernameValid = false
         }
         // return
       },
@@ -75,15 +79,15 @@
       },
       passwordIsValid(value) {
         const password = value
-        if (password.length >= 5 && password.length <= 20){
-          this.msg['password'] = '';
-          this.passwordValid = true;
-        } else{
-          this.msg['password'] = 'Måste innehålla 5-20 tecken';
-          this.passwordValid = false;
+        if (password.length >= 5 && password.length <= 20) {
+          this.msg['password'] = ''
+          this.passwordValid = true
+        } else {
+          this.msg['password'] = 'Måste innehålla 5-20 tecken'
+          this.passwordValid = false
         }
         // return password.length >= 5 && password.length <= 20
-      },
+      }
       // formIsValid() {
       //   return this.nameIsValid && this.emailIsValid && this.passwordIsValid
       // }
@@ -96,20 +100,33 @@
   <div class="wrapper">
     <label id="userlabel" for="username"
       >Användarnamn
-      <input v-model="s" id="username" type="text" placeholder="Användarnamn"
-    /><span class="warning-text" v-if="msg.username">{{ msg.username }}</span>
+      <input
+        v-model="s"
+        id="username"
+        type="text"
+        placeholder="Användarnamn"
+      /><span class="warning-text" v-if="msg.username">{{ msg.username }}</span>
     </label>
     <label id="elabel" for="email"
-      >email <input v-model="t" id="email" type="text" placeholder="Mail"
-    /><span class="warning-text" v-if="msg.email">{{ msg.email }}</span></label>
+      >email
+      <input v-model="t" id="email" type="text" placeholder="Mail" /><span
+        class="warning-text"
+        v-if="msg.email"
+        >{{ msg.email }}</span
+      ></label
+    >
 
     <label id="plabel" for="password"
       >Lösenord
-      <input v-model="u" id="password" type="password" placeholder="Lösenord"
-    /><span class="warning-text" v-if="msg.password">{{ msg.password }}</span>
+      <input
+        v-model="u"
+        id="password"
+        type="password"
+        placeholder="Lösenord"
+      /><span class="warning-text" v-if="msg.password">{{ msg.password }}</span>
     </label>
     <!-- -->
-    {{ !usernameValid }} {{ !emailValid }} {{ !passwordValid }}
+    <!-- {{ !usernameValid }} {{ !emailValid }} {{ !passwordValid }} -->
     <input
       :disabled="isRegisterDisabled"
       @click="regUser"
@@ -182,7 +199,7 @@
 
   #button:disabled {
     background-color: grey;
-    cursor: default
+    cursor: default;
   }
 
   #elabel,
