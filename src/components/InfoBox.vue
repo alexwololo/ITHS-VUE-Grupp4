037c944ecc668702906f99cd16162ff3b62ad27a
Anton Walther <anton.walther@live.com>
2022-03-02 14:36:50 +0100

    Merge branch 'main' of https://github.com/alexwololo/ITHS-VUE-Grupp4

---
+++ src/components/InfoBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="info">
+    <h1>Namn, xx år</h1>
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+    anim id est laborum.
+  </div>
+</template>
+
+<style scoped>
+  .info {
+    background-color: lightgrey;
+    width: 300px;
+    height: 300px;
+    border-radius: 40px;
+    margin: 25px;
+    padding: 20px;
+  }
+
+  h1 {
+    margin-top: 15px;
+  }
+</style>
