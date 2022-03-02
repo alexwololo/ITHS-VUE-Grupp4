7e37b214cdd6ab75f599b9610fd1fc0534617076
s0wie <sowieh@gmail.com>
2022-03-02 04:28:17 -0500

    changes

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -14,12 +14,15 @@
       </div>
     </div>
     <div id="right-side">
+      <div id="right-top">
+        <a href="/#/edit">edit profile</a>
+      </div>
       <div class="interests-container">
         <p class="profile-header">Intressen & Hobbies:</p>
         <ul>
           <li
             class="interests"
-            v-for="interest in this.$store.state.interests.split(', ')"
+            v-for="interest in this.$store.state.interests.split(',')"
             :key="interest.length"
           >
             {{ interest }}
@@ -134,6 +137,10 @@
     #right-side {
       margin-top: 2rem;
       margin-bottom: 1.5rem;
+    }
+
+    #right-top {
+      text-align: right;
     }
     .interests-container {
       border-bottom: 3px solid #e6e6e6;
